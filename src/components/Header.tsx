309836a164b3e23d554aba846e17e8da8c256b66
NicoAntonelli <niconelli2@gmail.com>
2023-09-21 09:44:36 +0200

    Header component placeholder

---
+++ src/components/Header.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { SocialIcon } from "react-social-icons"
+
+type Props = {}
+
+function Header({}: Props) {
+  return (
+    <header>
+        <div>
+            {/* Social Icons */}
+            <SocialIcon url='https://www.linkedin.com/in/nico-antonelli/' target='blank'/>
+        </div>
+    </header>
+  )
+}
+
+export default Header
