ab504324db63c054831ce6c2f7f87906dfd04cb3
NicoAntonelli <niconelli2@gmail.com>
2023-09-21 11:58:40 +0200

    Header: social icons and email

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,13 +5,29 @@ type Props = {}
 
 function Header({}: Props) {
     return (
-        <header>
-            <div>
+        <header className="flex justify-between p-5 max-w-7xl mx-auto z-20">
+            <div className="flex flex-row gap-2 items-center">
                 {/* Social Icons */}
                 <SocialIcon
                     url="https://www.linkedin.com/in/nico-antonelli/"
                     target="blank"
                 />
+                <SocialIcon
+                    url="https://github.com/NicoAntonelli/"
+                    target="blank"
+                />
+            </div>
+            <div className="flex flex-row gap-1 items-center cursor-pointer">
+                {/* Email */}
+                <SocialIcon
+                    url="mailto:niconelli2@gmail.com"
+                    network="email"
+                    bgColor="transparent"
+                    target="blank"
+                />
+                <p className="uppercase hidden md:inline-flex text-sm">
+                    Get in touch
+                </p>
             </div>
         </header>
     )
