b3dbacfe7872f05366f9fbfea3ce6900fbbcc975
NicoAntonelli <niconelli2@gmail.com>
2023-09-21 12:27:18 +0200

    Header: add framer motion transitions

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,24 +1,37 @@
+'use client'
+
 import React from 'react'
 import { SocialIcon } from 'react-social-icons'
+import { motion } from 'framer-motion'
 
 type Props = {}
 
 function Header({}: Props) {
     return (
         <header className="flex justify-between p-5 max-w-7xl mx-auto z-20">
-            <div className="flex flex-row gap-2 items-center">
-                {/* Social Icons */}
+            {/* Social Icons */}
+            <motion.div
+                className="flex flex-row gap-2 items-center"
+                initial={{ x: -500, opacity: 0, scale: 0.5 }}
+                animate={{ x: 0, opacity: 1, scale: 1 }}
+                transition={{ duration: 1.5 }}>
                 <SocialIcon
                     url="https://www.linkedin.com/in/nico-antonelli/"
+                    bgColor="transparent"
                     target="blank"
                 />
                 <SocialIcon
                     url="https://github.com/NicoAntonelli/"
+                    bgColor="transparent"
                     target="blank"
                 />
-            </div>
-            <div className="flex flex-row gap-1 items-center cursor-pointer">
-                {/* Email */}
+            </motion.div>
+            {/* Email */}
+            <motion.div
+                className="flex flex-row gap-1 items-center cursor-pointer"
+                initial={{ x: 500, opacity: 0, scale: 0.5 }}
+                animate={{ x: 0, opacity: 1, scale: 1 }}
+                transition={{ duration: 1.5 }}>
                 <SocialIcon
                     url="mailto:niconelli2@gmail.com"
                     network="email"
@@ -28,7 +41,7 @@ function Header({}: Props) {
                 <p className="uppercase hidden md:inline-flex text-sm">
                     Get in touch
                 </p>
-            </div>
+            </motion.div>
         </header>
     )
 }
