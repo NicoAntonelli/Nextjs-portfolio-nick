362b232a93f4b8fbae30f6ccfed5b12f3e5bfa4c
NicoAntonelli <niconelli2@gmail.com>
2023-09-22 13:30:41 +0200

    Page: make snap effect

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,14 @@
 import Header from '@/components/header/header'
 import Hero from '@/components/hero/hero'
-import Image from 'next/image'
 
 export default function Home() {
     return (
-        <main className="min-h-screen bg-[rgb(30,30,30)] text-white">
+        <main className="h-screen bg-[rgb(30,30,30)] text-white snap snap-y snap-mandatory overflow-scroll z-0">
             {/* Social Icons & Email */}
             <Header />
 
             {/* Hero */}
-            <section id="hero">
+            <section id="hero" className="snap-center">
                 <Hero />
             </section>
 
