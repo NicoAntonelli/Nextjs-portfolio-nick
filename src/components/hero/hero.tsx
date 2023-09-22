390c54fa7d6800f84fdb9b5b6454bd09907ea43c
NicoAntonelli <niconelli2@gmail.com>
2023-09-23 01:02:45 +0200

    Hero: add buttons & fix z-index

--- src/components/hero/hero.tsx
+++ src/components/hero/hero.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import { Cursor, useTypewriter } from 'react-simple-typewriter'
 import BackgroundCircles from './backgroundCircles'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Hero = () => {
     const [text, count] = useTypewriter({
@@ -31,7 +32,7 @@ const Hero = () => {
                     fill={true}
                 />
             </div>
-            <div>
+            <div className="z-20">
                 <h2 className="text-sm uppercase text-gray-500 pb-2 tracking-[15px]">
                     Software Engineer
                 </h2>
@@ -39,6 +40,20 @@ const Hero = () => {
                     <span className="mr-1">{text}</span>
                     <Cursor cursorColor="#e66c2c" />
                 </h1>
+                <div className="pt-5">
+                    <Link href="#about">
+                        <button className="heroButton">About</button>
+                    </Link>
+                    <Link href="#experience">
+                        <button className="heroButton">Experience</button>
+                    </Link>
+                    <Link href="#projects">
+                        <button className="heroButton">Projects</button>
+                    </Link>
+                    <Link href="#skills">
+                        <button className="heroButton">Skills</button>
+                    </Link>
+                </div>
             </div>
         </div>
     )
