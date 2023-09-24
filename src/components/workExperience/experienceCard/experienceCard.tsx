e1c5c14289921f957c37064f4797c7f7cae4d0a7
NicoAntonelli <niconelli2@gmail.com>
2023-09-24 13:25:48 +0200

    ExperienceCard: fix sizes

--- src/components/workExperience/experienceCard/experienceCard.tsx
+++ src/components/workExperience/experienceCard/experienceCard.tsx
@@ -5,9 +5,9 @@ import { motion } from 'framer-motion'
 
 const ExperienceCard = () => {
     return (
-        <article className="flex flex-col rounded-lg items-center p-10 space-y-7 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#222] cursor-pointer opacity-40 hover:opacity-100 transition-opacity duration-200 overflow-hidden">
+        <article className="flex flex-col rounded-lg items-center mt-20 p-5 space-y-2 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#222] cursor-pointer opacity-40 hover:opacity-100 transition-opacity duration-200 overflow-hidden">
             <motion.img
-                className="w-32 h-32 rounded-full xl:h-[200px] xl:w-[200px] object-cover object-center"
+                className="w-20 h-20 rounded-full xl:h-[100px] xl:w-[100px] object-cover object-center"
                 src="la_mutual_amr.jpg"
                 initial={{ y: -100, opacity: 0 }}
                 transition={{ duration: 1.2 }}
@@ -15,41 +15,45 @@ const ExperienceCard = () => {
                 viewport={{ once: true }}
             />
 
-            <div className="px-0 md:px-10">
+            <div className="px-5 md:px-10">
                 <h4 className="text-4xl font-light">Software Dev</h4>
                 <p className="text-2xl font-bold mt-1">La Mutual de AMR</p>
-                <div className="flex gap-2 space-x-2 my-2">
+                <div className="flex gap-2 space-x-1 my-1">
                     <Image
                         className="object-cover"
                         src={'/tech_js.png'}
                         alt={'JavaScript'}
-                        height={25}
-                        width={25}
+                        height={20}
+                        width={20}
                     />
                     <Image
                         className="object-cover"
                         src={'/tech_js.png'}
                         alt={'JavaScript'}
-                        height={25}
-                        width={25}
+                        height={20}
+                        width={20}
                     />
                     <Image
                         className="object-cover"
                         src={'/tech_js.png'}
                         alt={'JavaScript'}
-                        height={25}
-                        width={25}
+                        height={20}
+                        width={20}
                     />
                 </div>
-                <p className="uppercase py-5 text-gray-300">
+                <p className="uppercase py-3 text-gray-300">
                     Started work... - Ended...
                 </p>
 
-                <ul className="list-['✓'] space-y-2 ml-5 text-lg">
-                    <li className="pl-2">Summary points</li>
-                    <li className="pl-2">Summary points</li>
-                    <li className="pl-2">Summary points</li>
-                    <li className="pl-2">Summary points</li>
+                <ul className="list-['✓'] space-y-1 ml-5 text-base">
+                    <li className="pl-2">
+                        Two-lines max explanation about what things I did in
+                        this project for La Mutual de AMR
+                    </li>
+                    <li className="pl-2">
+                        Two-lines max explanation about what things I did in
+                        this project for La Mutual de AMR
+                    </li>
                     <li className="pl-2">Summary points</li>
                 </ul>
             </div>
