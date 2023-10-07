c6312ec02ae551b80e7040777915be7f5b281d49
NicoAntonelli <niconelli2@gmail.com>
2023-10-07 13:20:28 +0200

    Routes: add type to social get, and its own folder

--- src/app/api/social/route.ts
+++ src/app/api/social/route.ts
@@ -1,17 +1,13 @@
 import { type NextRequest, NextResponse } from 'next/server'
 import { groq } from 'next-sanity'
 import { client } from '../../../../sanity/lib/client'
-
-type Data = {
-    socials: any
-}
+import Social from '@/entities/Social'
 
 const query: string = groq`
     *[_type == "social"]
 `
 
 export async function GET(req: NextRequest) {
-    const response: any = await client.fetch(query)
-
+    const response: Social[] = await client.fetch(query)
     return Response.json(response, { status: 200 })
 }
