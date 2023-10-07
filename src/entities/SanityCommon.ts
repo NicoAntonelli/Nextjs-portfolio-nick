524a10f09433a4d3441ac78adb52a7e65983851b
NicoAntonelli <niconelli2@gmail.com>
2023-10-07 12:20:22 +0200

    Entities: add common sanity ones

---
+++ src/entities/SanityCommon.ts
@@ -0,0 +1,8 @@
+interface SanityCommon {
+    _createdAt: string
+    _id: string
+    _rev: string
+    _updatedAt: string
+}
+
+export default SanityCommon
---
+++ src/entities/SanityImage.ts
@@ -0,0 +1,9 @@
+interface SanityImage {
+    _type: 'Image'
+    asset: {
+        _ref: string
+        _type: 'Reference'
+    }
+}
+
+export default SanityImage
