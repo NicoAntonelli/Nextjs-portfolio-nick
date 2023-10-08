2ac9bad01e3af004e8f0d3a83f7f6982b2d751bb
Nico Antonelli <niconelli2@gmail.com>
2023-10-09 00:26:17 +0200

    Images: set next config and extend sanity url getter

--- sanity/lib/image.ts
+++ sanity/lib/image.ts
@@ -1,13 +1,14 @@
 import createImageUrlBuilder from '@sanity/image-url'
 import type { Image } from 'sanity'
-
 import { dataset, projectId } from '../env'
 
+import SanityImage from '@/entities/Base/SanityImage'
+
 const imageBuilder = createImageUrlBuilder({
-  projectId: projectId || '',
-  dataset: dataset || '',
+    projectId: projectId || '',
+    dataset: dataset || '',
 })
 
-export const urlForImage = (source: Image) => {
-  return imageBuilder?.image(source).auto('format').fit('max')
+export const urlForImage = (source: Image | SanityImage) => {
+    return imageBuilder?.image(source).auto('format').fit('max')
 }
