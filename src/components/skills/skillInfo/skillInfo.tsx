5648c4768a354f958829933b3142ec6f914bb859
NicoAntonelli <niconelli2@gmail.com>
2023-10-13 01:30:13 +0200

    Skills: fix framer motion bug

--- src/components/skills/skillInfo/skillInfo.tsx
+++ src/components/skills/skillInfo/skillInfo.tsx
@@ -14,13 +14,13 @@ const SkillInfo = ({ skill, directionLeft }: Props) => {
     return (
         <div className="group relative flex cursor-pointer">
             <motion.img
-                className="w-8 h-8 md:h-10 md:w-10 xl:w-12 xl:h-12 rounded-full border border-gray-500 object-cover filter group-hover:grayscale transition duration-300 ease-in-out"
+                className="w-6 h-6 md:h-9 md:w-9 xl:w-12 xl:h-12 rounded-full border border-gray-500 object-cover filter group-hover:grayscale transition duration-300 ease-in-out"
                 src={urlForImage(skill.image).url()}
-                initial={{ x: directionLeft ? -100 : 100, opacity: 0 }}
+                initial={{ x: directionLeft ? -80 : 80, opacity: 0 }}
                 transition={{ duration: 1 }}
                 whileInView={{ x: 0, opacity: 1 }}
             />
-            <div className="absolute h-8 w-8 md:h-10 md:w-10 xl:w-12 xl:h-12 rounded-full opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-gray-900">
+            <div className="absolute h-6 w-6 md:h-9 md:w-9 xl:w-12 xl:h-12 rounded-full opacity-0 group-hover:opacity-80 transition duration-300 ease-in-out group-hover:bg-gray-900">
                 <div className="flex items-center justify-center h-full">
                     <p className="text-xs md:text-sm xl:text-base font-bold text-[#e66c2c] text-center opacity-100">
                         {skill.title}
